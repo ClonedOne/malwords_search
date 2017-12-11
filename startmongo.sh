36b5b7cb1fb4fddef4199ac92f49482b1902ff5c
ClonedOne <severi.giorgio@gmail.com>
2017-12-11 19:38:40 +0100

    new stuff

--- startmongo.sh
+++ startmongo.sh
@@ -1,3 +1,9 @@
 !#/bin/bash
 
-docker run -d -p 27017:27017 -p 28017:28017 --name mongodb -v /home/ubuntu/composetest/data/small:/data/db:ro mongo
+docker run -d -p 27017:27017 -p 28017:28017 --name mongodb -v /home/ubuntu/composetest/db:/data/db -v /home/ubuntu/data:/data/jsons:ro mongo
+
+
+docker exec -it mongodb mongo admin
+
+db.createUser({ user: 'dbadmin', pwd: '', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
+

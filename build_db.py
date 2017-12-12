5effc1c1ed2b12bdc53b2298cf76020af6b3dbbb
Ubuntu <ubuntu@ip-172-31-41-134.us-east-2.compute.internal>
2017-12-12 02:31:05 +0000

    stuff

--- build_db.py
+++ build_db.py
@@ -1,20 +1,30 @@
 import urllib.parse
 import pymongo
+import json
 import sys
+import os
 
 
 
-if len(sys.argv < 2):
+if len(sys.argv) < 4:
     print('Please specify data folder, user name, and password')
     exit(1)
 
 data_path = sys.argv[1]
 username = urllib.parse.quote_plus(sys.argv[2])
 password = urllib.parse.quote_plus(sys.argv[3])
-client = MongoClient(
+client = pymongo.MongoClient(
     username=username,
     password=password,
 )
 
+db = client['samples']
+words = db.words
+
+for json_file in os.listdir(data_path):
+    sample_words = json.load(open(os.path.join(data_path, json_file), 'r'))
+    sample_words['_id'] = json_file.split('.')[0].strip()
+    words_id = words.insert_one(sample_words)
+    print(words_id, words['_id'])
 
 

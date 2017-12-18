4098571ffec369bf5b445370a51be84c2673aa41
ClonedOne <severi.giorgio@gmail.com>
2017-12-18 16:18:43 +0100

    fixing db auth

--- build_db.py
+++ build_db.py
@@ -12,7 +12,8 @@ if len(sys.argv) < 4:
 
 data_path = sys.argv[1]
 username = urllib.parse.quote_plus(sys.argv[2])
-password = urllib.parse.quote_plus(sys.argv[3])
+password = urllib.parse.quote_plus(open(sys.argv[3], 'r').readline().strip())
+print(password)
 client = pymongo.MongoClient(
     username=username,
     password=password,
--- startmongo.sh
+++ startmongo.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# docker run -d -p 27017:27017 -p 28017:28017 --name mongodb -v /home/ubuntu/db:/data/db -v /home/ubuntu/data:/data/jsons:ro mongo --auth
-
 echo "Starting Docker container"
+
+mongoadminpwd=$(head -n 1 .adminpwd)
+mongouserpwd=$(head -n 1 .userpwd)
+echo $mongoadminpwd
 
 docker run -it \
     -e MONGODB_ADMIN_USER=admin \
-    -e MONGODB_ADMIN_PASS= \
+    -e MONGODB_ADMIN_PASS=$mongoadminpwd \
     -e MONGODB_APPLICATION_DATABASE=mytestdatabase \
     -e MONGODB_APPLICATION_USER=testuser \
-    -e MONGODB_APPLICATION_PASS=testpass \
+    -e MONGODB_APPLICATION_PASS=$mongouserpwd \
     -p 27017:27017 \
     -p 28017:28017 \
-    -v /home/ubuntu/db:/data/db \
-    -v /home/ubuntu/data:/data/jsons:ro \
+    -v /home/gio/projects/malwords_search/db:/data/db \
+    -v /media/gio/secondary/data/malwords/small:/data/jsons:ro \
     --name mongodb mymongo

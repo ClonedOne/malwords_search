87a0554c482ff79794b9ed0876dbc67af09675c1
ClonedOne <severi.giorgio@gmail.com>
2018-02-13 03:40:20 +0100

    Fixes

--- indexing_settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -XPUT 'localhost:9200/malwords/_settings' -d ' {
-    "index" : {
-    	"refresh_interval" : "-1",
-        "number_of_replicas" : 0
-    }
-}'
--- restartall.sh
+++ restartall.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 docker-compose up -d --build
-sleep 5
-docker exec mw-api "node" "server/connection.js"
-sleep 5
-docker exec mw-api "node" "server/load_data.js"
-sleep 5
+sleep 10
+python 'load_data.py'
+sleep 10
 docker-compose logs -f api

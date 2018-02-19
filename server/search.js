470d3f5bb8eb041733ecf51774f2b39cafc44420
ClonedOne <severi.giorgio@gmail.com>
2018-02-19 01:44:06 +0100

    different search mode

--- server/search.js
+++ server/search.js
@@ -3,19 +3,37 @@ const { client, index, type } = require('./connection')
 module.exports = {
   /** Query ES index for the provided term */
   queryTerm (term, offset = 0) {
+
+    query_body = {
+      "multi_match" : {
+          "query" : term,
+          "fields": ["content", "raw"],
+          "fuzziness": 0
+      } 
+    }
+
+    if (term.split(':')[0].trim() === 'sys'){
+      query_body = {
+        "match" : {
+            "syscalls" : term
+        }
+      }
+    } else if (term.split(':')[0].trim() === 'reg'){
+      query_body = {
+        "match" : {
+            "registry" : term
+        }
+      }
+    }
+
     const body = {
       from: offset, 
       "_source": {
           "exclude": ["content", "syscalls", "raw", "registry"]
       },
 
-      "query": {
-        "multi_match" : {
-            "query" : term,
-            "fields": ["content", "syscalls", "raw", "registry"],
-            "fuzziness": 0
-        } 
-      },
+      "query": query_body,
+
       highlight: { fields: { content: {} } }
     }
 
